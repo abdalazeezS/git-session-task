386b4fc965f7bc1d7229363165806c60b6f39b79
MajdTamimi <majd2004tamimi@gmail.com>
2024-09-28 20:20:34 +0300

    Add Majd card

--- data/index.js
+++ data/index.js
@@ -5,5 +5,12 @@ export const profiles = [
         gender: "male",
         linkedInUserName: "abdalazeez-shahateet",
         gitHubUserName: "abdalazeezS"
+    },
+    {
+        name: "Majd Tamimi",
+        jobTitle: "Frontend Developer",
+        gender: "female",
+        linkedInUserName: "Majd Tamimi",
+        gitHubUserName: "MajdTamimi"
     }
 ]
