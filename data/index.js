d9d5a9fc8289e9aa5d79b36b2ab354a4a8dc3b93
barahaw <barahawamdeh2003@gmail.com>
2024-09-28 21:51:46 +0300

    create Bara's Card

--- data/index.js
+++ data/index.js
@@ -1,25 +1,32 @@
 export const profiles = [
-    {
-        name: "Abdalazeez Shahateet",
-        jobTitle: "Frontend Developer",
-        gender: "male",
-        linkedInUserName: "abdalazeez-shahateet",
-        gitHubUserName: "abdalazeezS"
-    },
-    {
-        name: "Majd Tamimi",
-        jobTitle: "Frontend Developer",
-        gender: "female",
-        linkedInUserName: "Majd Tamimi",
-        gitHubUserName: "MajdTamimi"
-    },
-    {
-        name: "Mohammad Hmedat",
-        jobTitle: "Mobile  App Developer",
+  {
+    name: "Abdalazeez Shahateet",
+    jobTitle: "Frontend Developer",
+    gender: "male",
+    linkedInUserName: "abdalazeez-shahateet",
+    gitHubUserName: "abdalazeezS",
+  },
+  {
+    name: "Majd Tamimi",
+    jobTitle: "Frontend Developer",
+    gender: "female",
+    linkedInUserName: "Majd Tamimi",
+    gitHubUserName: "MajdTamimi",
+  },
+  {
+    name: "Mohammad Hmedat",
+    jobTitle: "Mobile  App Developer",
 
-        gender: "Male",
-        linkedInUserName: "mohammad-hmedat",
-        gitHubUserName: "MohammadHumedat"
-    }
+    gender: "Male",
+    linkedInUserName: "mohammad-hmedat",
+    gitHubUserName: "MohammadHumedat",
+  },
+  {
+    name: "Baraa Hawamdeh",
+    jobTitle: "Frontend Developer",
 
-]
+    gender: "Male",
+    linkedInUserName: "bara-hawamdeh",
+    gitHubUserName: "barahaw",
+  },
+];
